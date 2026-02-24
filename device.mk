3a7130858b6dbb1ea6bdba95d9b09ef1c080023b
HzzMonet <hzzmonet7@gmail.com>
2026-02-25 00:52:13 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,35 +1,18 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/xiaomi/nuwa
+DEVICE_PATH := device/xiaomi/nuwa
+
 # A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+AB_OTA_UPDATER := true
 
-# Boot control HAL
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Boot HAL (AIDL)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot-service.default
 
-PRODUCT_PACKAGES += \
-    bootctrl.kalama
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.kalama \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+# Soong namespace
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
