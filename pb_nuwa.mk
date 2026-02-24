71e148c227c8ad834f6daa5025ff3cbb9425a6f8
HzzMonet <hzzmonet7@gmail.com>
2026-02-25 06:49:37 +0700

    Update product configuration for pb_nuwa

--- pb_nuwa.mk
+++ pb_nuwa.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from nuwa device
 $(call inherit-product, device/xiaomi/nuwa/device.mk)
 
 PRODUCT_DEVICE := nuwa
-PRODUCT_NAME := omni_nuwa
+PRODUCT_NAME := pb_nuwa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2210132C
 PRODUCT_MANUFACTURER := xiaomi
